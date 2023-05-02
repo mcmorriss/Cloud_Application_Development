854b95aa5ecd2bfba58e6dc6f38ddd7187cc1345
Michael Morriss <61882610+mcmorriss@users.noreply.github.com>
2023-05-02 01:38:08 -0500

    Create datastore.js

---
+++ Project-4-Intermediate-API/datastore.js
@@ -0,0 +1,10 @@
+const projectId = 'project4-morrism4';
+
+const {Datastore} = require('@google-cloud/datastore');
+
+module.exports.Datastore = Datastore;
+module.exports.datastore = new Datastore();
+module.exports.fromDatastore = function fromDatastore(item){
+    item.id = item[Datastore.KEY].id;
+    return item;
+}
